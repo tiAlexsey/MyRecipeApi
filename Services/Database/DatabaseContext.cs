25f3dcda4d28218146730e473c1f060a9787823e
tialexsey <tiAlexsey@gmail.com>
2023-08-25 20:28:28 +0300

    added context

---
+++ Services/Database/DatabaseContext.cs
@@ -0,0 +1,37 @@
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace Services.Database;
+
+public partial class DatabaseContext : DbContext
+{
+    public DatabaseContext()
+    {
+    }
+
+    public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Ingredient> Ingredient { get; set; } = null!;
+    public DbSet<Dish> Dish { get; set; } = null!;
+    public DbSet<LinkIngredients> LinkIngredients { get; set; } = null!;
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder.EnableSensitiveDataLogging(true);
+            optionsBuilder.UseNpgsql();
+            optionsBuilder.LogTo(Console.WriteLine, LogLevel.Information);
+        }
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
