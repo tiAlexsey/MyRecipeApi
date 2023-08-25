2658a9dfcadbef7329277cf5c3045252cec7c0b3
tialexsey <tiAlexsey@gmail.com>
2023-08-25 20:50:45 +0300

    Added root index.html & refactor program.cs

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -3,26 +3,16 @@ using Microsoft.EntityFrameworkCore;
 using Services.Database;
 using Services.Database.Repository;
 
-var builder = WebApplication.CreateBuilder(args);
-
-var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 var config = new ConfigurationBuilder()
     .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
     .AddJsonFile("appsettings.json")
     .Build();
 
-builder.Services.AddCors(options => {
-    options.AddPolicy(name: MyAllowSpecificOrigins,
-        builder => {
-            builder.WithOrigins("http://localhost:8080").AllowAnyMethod().AllowAnyHeader();
-            builder.WithOrigins("http://192.168.68.100:8080").AllowAnyMethod().AllowAnyHeader();
-        });
-});
-
+var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
+builder.Services.AddCors();
 builder.Services.AddDbContext<DatabaseContext>(options =>
-    options.UseNpgsql(config.GetConnectionString("DefaultConnection"))
-);
+    options.UseNpgsql(config.GetConnectionString("DefaultConnection")));
 builder.Services.AddScoped<IIngredientRepository, IngredientRepository>();
 builder.Services.AddScoped<IRecipeRepository, RecipeRepository>();
 builder.Services.AddScoped<IDishRepository, DishRepository>();
@@ -32,18 +22,15 @@ builder.Services.AddSwaggerGen(options => { options.EnableAnnotations(); });
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseCors(options => { options.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod(); });
 
-app.UseCors(MyAllowSpecificOrigins);
+app.UseSwagger();
+app.UseSwaggerUI();
 
-app.UseHttpsRedirection();
+app.UseDefaultFiles();
+app.UseStaticFiles();
 
-app.UseAuthorization();
+app.UseRouting();
 
 app.MapControllers();
 
